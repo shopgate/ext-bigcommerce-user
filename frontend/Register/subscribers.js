77719764eb0136d38b1d4394be534bf1d4b1637f
Szymon Nowicki <sznowicki@users.noreply.github.com>
2019-02-21 13:50:12 +0100

    PWA6 migration (#14)
    BIGC-794 - PWA6 frontend migration + version bump (2.0.0)

--- frontend/Register/subscribers.js
+++ frontend/Register/subscribers.js
@@ -1,28 +1,78 @@
+import { REGISTER_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
+import { redirects } from '@shopgate/pwa-common/collections';
+import { getCurrentRoute } from '@shopgate/pwa-common/helpers/router';
+import { registerRedirect } from '@shopgate/pwa-webcheckout-shopify/action-creators/register';
+import { webCheckoutRegisterRedirect$ } from '@shopgate/pwa-webcheckout-shopify/streams';
+import { userDidLogin$ } from '@shopgate/pwa-common/streams/user';
+import buildRegisterUrl from '@shopgate/pwa-common/subscriptions/helpers/buildRegisterUrl';
+import { routeDidEnter$ } from '@shopgate/pwa-common/streams';
+import closeInAppBrowser from '@shopgate/pwa-core/commands/closeInAppBrowser';
+import broadcastEvent from '@shopgate/pwa-core/commands/broadcastEvent';
+import { isAndroid } from '@shopgate/pwa-common/selectors/client';
+import { historyPop, historyPush } from '@shopgate/pwa-common/actions/router';
 import fetchRegisterUrl from '@shopgate/pwa-common/actions/user/fetchRegisterUrl';
-import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
 import { getRegisterUrl } from '@shopgate/pwa-common/selectors/user';
-import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link';
-import { openedRegisterLink$ } from '@shopgate/pwa-common/streams/history';
-import openRegisterUrl from '@shopgate/pwa-common/subscriptions/helpers/openRegisterUrl';
-import { LEGACY_URL } from '@shopgate/pwa-common/constants/Registration';
+import { appWillStart$ } from '@shopgate/pwa-common/streams/app';
+
+/**
+ * @param {Object} params The handler parameters.
+ * @param {Function} params.dispatch The Redux dispatch function.
+ * @param {Function} params.getState The Redux getState function.
+ * @return {Promise<string>}
+ */
+const redirectHandler = async ({ dispatch, getState }) => {
+  /**
+   * When the register url was opened from a login page, a redirect to the original target
+   * page needs to happen after a successful registration. It's added by buildRegisterUrl.
+   */
+  const { state: { redirect: { location = '' } = {} } } = getCurrentRoute();
+
+  let url = getRegisterUrl(getState());
+
+  if (!url) {
+    // Fetch a fresh url if none was found within the store.
+    url = await dispatch(fetchRegisterUrl());
+  }
+  url = buildRegisterUrl(url, location);
+
+  // Dispatch redirect
+  dispatch(registerRedirect(url));
+
+  return url;
+};
 
 export default (subscribe) => {
-  // Open register link subscription
-  subscribe(openedRegisterLink$, async ({ dispatch, getState }) => {
-    const state = getState();
+  subscribe(appWillStart$, () => {
+    redirects.set(REGISTER_PATH, redirectHandler, true);
+  });
 
-    const hasRegistrationUrl = !!getRegisterUrl(state);
+  const loginAfterRegisterRedirect$ = webCheckoutRegisterRedirect$
+    .switchMap(() => userDidLogin$.first());
+  const nextRouterAfterLoginRegister$ = loginAfterRegisterRedirect$
+    .switchMap(() => routeDidEnter$.first())
+    .debounceTime(1000);
 
-    // Open the registration url if one is found.
-    if (hasRegistrationUrl) {
-      await dispatch(fetchRegisterUrl())
-        .then(url => openRegisterUrl(url, state))
-        .catch(e => e);
-    } else {
-      const link = new ParsedLink(LEGACY_URL);
-      link.open();
+  /**
+   * Pop a login page after web registration / and redirect to checkout
+   */
+  subscribe(loginAfterRegisterRedirect$, ({ dispatch, getState }) => {
+    const { state: { redirect: { location = '' } = {} } } = getCurrentRoute();
+    dispatch(historyPop());
+
+    closeInAppBrowser(isAndroid(getState()));
+
+    if (location) {
+      dispatch(historyPush({
+        pathname: location,
+      }));
     }
+  });
 
-    dispatch(goBackHistory(1));
+  /**
+   * Close loading spinner on next after login route
+   */
+  subscribe(nextRouterAfterLoginRegister$, () => {
+    // Close loading view
+    broadcastEvent({ event: 'closeNotification' });
   });
 };
