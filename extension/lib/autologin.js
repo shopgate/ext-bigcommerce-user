d1ee641bc67d4348566cb06473a7f040be5a8c79
Konstantin Kiritsenko <k@componentk.com>
2019-03-04 19:27:15 +0700

    BIGC-801 Fix per review
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- extension/lib/autologin.js
+++ extension/lib/autologin.js
@@ -1,6 +1,7 @@
 const { decorateError } = require('./shopgate/logDecorator')
 const { getCustomer } = require('./shopgate/customer/get')
 const UserNotFoundError = require('./shopgate/customer/errors/UserNotFoundError')
+const InvalidTokenReceivedError = require('./shopgate/customer/errors/InvalidTokenReceivedError')
 
 /**
  * @param {PipelineContext} context
@@ -8,6 +9,10 @@ const UserNotFoundError = require('./shopgate/customer/errors/UserNotFoundError'
  * @param {BigCommerceCurrentCustomer} input.customer
  */
 module.exports = async (context, input) => {
+  if (!input.customer.email) {
+    throw new InvalidTokenReceivedError()
+  }
+
   try {
     return await getCustomer(context, input.customer.email)
   } catch (err) {
